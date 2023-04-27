2eaed369002185a43ea5acfbf4c06a8d49d96610
Aleksander Wójtowicz <a.wojtowicz@avsystem.com>
2023-04-27 22:54:08 +0200

    attempt to use media list player

--- script.py
+++ script.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python3
 
-import sys, vlc, time
-
-def main():
-	if len(sys.argv) <= 1:
-		print('Brak filmow', file=sys.stderr)
-		return 1
-
-	movies = sys.argv[1:]
-
-	inst = vlc.Instance()
-	player = inst.media_player_new()
-
-	for movie in movies:
-		media = inst.media_new(movie)
-		player.set_media(media)
-		player.play()
-		time.sleep(2)
-		player.pause()
-		time.sleep(3)
-		player.pause()
-		time.sleep(9999)
-
+import sys, vlc, time, argparse
 
 if __name__ == '__main__':
-	exit(main())
+	parser = argparse.ArgumentParser(description='VLC Control')
+	parser.add_argument(
+		'--mrl-set',
+		dest='mrl_sets',
+		metavar='mrl',
+		help='List of Media Resource Locators forming a set of resources',
+		nargs='+',
+		action='append',
+		default=[]
+	)
+
+	args = parser.parse_args()
+	if len(args.mrl_sets) == 0:
+		print('No Media Resource Locator sets specified', file=sys.stderr)
+		exit(1)
+
+	for i, mrl_set in enumerate(args.mrl_sets):
+		stringified_set = ', '.join(mrl_set)
+		print(f'MRL set {i}: {stringified_set}')
+
+	inst = vlc.Instance()
+	list_player = inst.media_list_player_new()
+	media_lists = [inst.media_list_new(mrl_set) for mrl_set in args.mrl_sets]
+
+	while True:
+		for i, media_list in enumerate(media_lists):
+			print(f'Playing media list no {i} in 1 sec for 3 secs')
+			# list_player.stop()
+
+			list_player.set_media_list(media_list)
+			time.sleep(1)
+
+			list_player.play()
+			list_player.set_pause(0)
+			time.sleep(3)
+			list_player.set_pause(1)
